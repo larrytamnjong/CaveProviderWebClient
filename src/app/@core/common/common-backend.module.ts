78e5a00a74ee69e5fcc72d875ee13ec5b819896f
tamnjongLarry <larrytabeh@gmail.com>
2024-08-26 18:49:03 +0100

    Minor import cleaning and unused roles delete

--- src/app/@core/common/common-backend.module.ts
+++ src/app/@core/common/common-backend.module.ts
@@ -3,10 +3,7 @@ import { ModuleWithProviders, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UsersApi } from '../api/users.api';
 import { HttpService } from '../api/http.service';
-import { CountryData } from '../interfaces/common/countries';
-
 import { NbAuthModule } from '@nebular/auth';
-import { SettingsData } from '../interfaces/common/settings';
 import { UserData } from '../interfaces/common/users';
 import { UsersService } from '../services/users.service';
 import { PermissionApi } from '../api/permission.api';
