f7b35020b4b624d228ffa839ab9adffb18f2484b
tamnjongLarry <larrytabeh@gmail.com>
2024-09-10 14:12:45 +0100

    Removed more redundant items

--- src/app/@components/components.module.ts
+++ src/app/@components/components.module.ts
@@ -4,13 +4,10 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { NgxValidationMessageComponent } from './validation-message/validation-message.component';
-import {
-  NgxFilterByNumberComponent,
-} from './custom-smart-table-components/filter-by-number/filter-by-number.component';
 
 const COMPONENTS = [
   NgxValidationMessageComponent,
-  NgxFilterByNumberComponent,
+  
 ];
 
 @NgModule({
@@ -22,7 +19,7 @@ const COMPONENTS = [
   exports: [...COMPONENTS],
   declarations: [...COMPONENTS],
   entryComponents: [
-    NgxFilterByNumberComponent,
+    
   ],
 })
 export class ComponentsModule {
